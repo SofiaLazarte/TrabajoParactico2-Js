2e1510b41e92830a7e677fc71c20f19914d9a759
Sofia Lazarte <sofi.lazarte.sl@gmail.com>
2023-05-25 20:02:09 -0300

    correccion punto cinco

--- archivo.js
+++ archivo.js
@@ -110,47 +110,45 @@ else
 }
 
 document.write(numero);
-
+*/
 
 //punto cinco ---------------------------------------------------------------------------------------------------
 
 
 let letras = "TRWAGMYFPDXBNJZSQVHLCKE";
-let documento = parseInt(prompt("Escriba su DNI"));
-let resto = documento % 23;
 
 
-if (documento === null) 
-{ 
-    while (documento >=0 && documento < 99999999) 
+do {
+
+    let documento = parseInt(prompt("Escriba su DNI"));
+    if (documento >= 0 && documento <= 99999999) 
     {
-        
-        
+        let resto = documento % 23;
         let l = letras.charAt(resto);
         alert(l);
-        break;
+    }
+    else 
+    {
+        alert("ingrese un numero valido");
     }
 
-}
-else 
-{
-    alert("Escriba un numero valido");
-    
-}
-
-*/
+} while (confirm("quiere serguir ingresando") == true);
 
 
+
+/*
 // punto seis
 
-for (let i = 1; i<=30; i++) 
+for (let i = 1; i <= 30; i++) 
 {
+    document.write(i);
 
     let result = i + "" ; //convierte el numero i en cadena de texto y una vez hecho eso se usa repeat ya que solo lo hace con texto. Cuando concatenas un número con una cadena vacía (""), JavaScript automáticamente realiza una conversión implícita del número a una cadena.
 
     let secuencia = result.repeat(i)
 
-    document.write ( secuencia + "<br>") ;
+    document.write(secuencia + "<br>");
     //document.write() tenemos que usar el mismo caracter que en HTML, el "<br>" . El document.write() lo que va a hacer es escribir en HTML el código que pasemos a la función, por ese motivo hay que usar el "<br>". Si el salto de línea lo quisiésemos incluir en un alert() entonces tendríamos que usar el caracter "\n" para que hiciese el salto de línea, que es el caracter que hay que usar para hacer el salto de línea en javascript
 }
-
+*/
+// punto siete 
